c11c9e0584a3763a00d30a7ebc669f33f04ad2a3
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-08-07 14:00:26 -0300

    created inputs text, search and apllying css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 import Axios from 'axios'
+import { useState } from 'react'
 
 function App() {
-   const url = `https://api.edamam.com/search?q=chicken&app_id=acd0ba84&app_key=e1ad733c08b6c44284572637b40e3167`;
+   const [query, setQuery] = useState("")
+  
+  
+   const YOUR_APP_ID = 'acd0ba84'
+   const YOUR_APP_KEY = 'e1ad733c08b6c44284572637b40e3167'
+   const url =  `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&&health=alcohol-free`;
 
   async function getRecipies(){
       var result = await Axios.get(url)
@@ -10,9 +16,17 @@ function App() {
    }
 
   return (
-    <div className="App">
-      <h1 onClick={getRecipies} >Hellow</h1>
-      
+    <div className="app">
+      <h1 onClick={getRecipies}>Food Recipe Plaza</h1>
+        <form className="app__searchForm">
+            <input type="text" 
+                   className='app__input'
+                   placeholder='enter ingridient' 
+                   value={query}
+                   onChange={(e) => setQuery(e.target.value)}
+                    />
+            <input className="app__submit" type="submit" value="Search"></input>
+        </form>
     </div>
   );
 }
